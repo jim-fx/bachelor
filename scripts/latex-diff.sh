887cacc43ed5e9fe74621cbad4eeafb8bbd75092
Max Richter <jim-x@web.de>
2024-01-11 21:22:46 +0100

    feat: change start date (release)

--- scripts/latex-diff.sh
+++ scripts/latex-diff.sh
@@ -13,21 +13,25 @@ if [ "$CI" == "true" ]; then
   git config --global --add safe.directory "*"
 fi
 
+# Find the SHA of the last commit with the commit message containing "release"
+last_release_commit=$(git log --grep="release" -n 1 --format="%H")
+
+# If no commit with "release" in the message is found, set it to the previous commit
+if [ -z "$last_release_commit" ]; then
+    last_release_commit=$(git log -n 2 --skip=1 --format="%H")
+fi
+
 # Use git show to get the content of the previous version and save it to file-old
-git show HEAD^:"$file.tex" > "old-$file.tex"
-
-
+git show "$last_release_commit":"$file.tex" > "old-$file.tex"
 
 # Generate the latexdiff
 latexdiff "old-$file.tex" "$file.tex" --flatten > "diff-$file.tex"
 
-biber "diff-$file"
 make pdf FILE="diff-$file"
 
-mv diff-"$file".pdf "$file"-diff.pdf
+mv "diff-$file.pdf" "$file-diff.pdf"
 
 # Clean up temporary files
-rm old-$file.tex diff-$file.tex
+rm "old-$file.tex" "diff-$file.tex"
 
 echo "Latex-diff complete. Output saved to $file-diff.pdf."
-
